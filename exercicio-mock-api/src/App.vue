3cc59074d1e738c7bb5ff15df7acc9c6b169a12f
Kaua <kauakirchner2@gmail.com>
2022-05-13 15:19:18 -0300

    Exercicio3-modulo1-semana11
    Em progresso, dando not found quando consulta na api por id

---
+++ exercicio-mock-api/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <MockApi/>
+  </div>
+</template>
+
+<script>
+import MockApi from './components/MockApi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MockApi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ exercicio-mock-api/src/components/MockApi.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+      <h1>Consulta em api por id</h1>
+      <input type="number" v-model="id">
+      <div class="btn">
+          <button @click="consultaApi">Consultar</button>
+      </div>
+      {{user}}
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+export default {
+    name: 'MockApiA',
+
+    data(){
+        return{
+            id: null,
+            user:{} 
+        }
+    },
+    methods:{
+        async consultaApi(){
+            const promisse = axios.get(`https://627e989bb75a25d3f3baf486.mockapi.io/api/v1/users/${this.id}`)
+            await promisse.then((response) =>{
+            this.user = response.data
+            console.log(response.data)
+            }).catch((err) =>{
+                console.log(err)
+                alert('Mano deu erro')
+            })
+        },
+    }
+}
+
+</script>
+
+<style>
+
+</style>
