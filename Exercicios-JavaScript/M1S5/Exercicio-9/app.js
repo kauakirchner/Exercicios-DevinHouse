54aa48a95d4c37273191b93ef461fc2a7eea5d7e
Kaua <kauakirchner2@gmail.com>
2022-05-14 19:35:37 -0300

    Requisito de Inserir na tela que estava faltando

--- Exercicios-JavaScript/M1S5/Exercicio-9/app.js
+++ Exercicios-JavaScript/M1S5/Exercicio-9/app.js
@@ -1,9 +1,13 @@
+
+
 async function consultaCep(){
     const cep = document.getElementById('cep').value;
     const url = `https://viacep.com.br/ws/${cep}/json/`;
     const result = document.getElementById('retorno');
     const consulta = await fetch(url);
     const enderecoRetornado = await consulta.json()
-    console.log(enderecoRetornado)
+    result.innerText = JSON.stringify(enderecoRetornado)
         
 }
+
+
