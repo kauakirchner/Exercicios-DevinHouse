20c469533c93afd9b9028280b708ff77d36334ac
Kaua <kauakirchner2@gmail.com>
2022-05-14 19:21:51 -0300

    M1S5 exercicio
    Exercicio que consiste em consumir a Api viaCep. Utilizei o fetch

---
+++ Exercicios-JavaScript/M1S5/Exercicio-9/app.js
@@ -0,0 +1,9 @@
+async function consultaCep(){
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    const result = document.getElementById('retorno');
+    const consulta = await fetch(url);
+    const enderecoRetornado = await consulta.json()
+    console.log(enderecoRetornado)
+        
+}
