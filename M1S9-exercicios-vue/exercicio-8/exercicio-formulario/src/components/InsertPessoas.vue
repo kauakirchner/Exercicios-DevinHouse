84470c823028e767f34e983bd7ac119033c9b81f
Kaua <kauakirchner2@gmail.com>
2022-05-16 18:25:31 -0300

    M1S9 Exercício-8
    Exercício de cadastro de pessoas.

---
+++ M1S9-exercicios-vue/exercicio-8/exercicio-formulario/src/components/InsertPessoas.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+      <div class="Form">
+        <form>
+            <input type="text" placeholder="Nome" class="form-control" v-model="pessoa.nome">
+            <hr>
+            <input type="number" placeholder="Idade" class="form-control" v-model="pessoa.idade">
+        </form>
+        <hr>
+        <button type="button" class="btn btn-primary" @click="insertPessoa">Inserir</button>
+        <hr>
+      </div>
+      <div class="row">
+          <table>
+              <thead>
+                <tr>
+                <th class="col-1">Nome</th>
+                <th class="col-1">Idade</th>
+                </tr>
+              </thead>
+              <tbody>
+                  <tr v-for="i in pessoas" :key="i">
+                    <td v-text="pessoa.nome"></td>
+                    <td v-text="pessoa.idade"></td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'InsertPessoa',
+    data(){
+        return{
+            pessoas:[],
+            pessoa:{}
+        }
+    },
+    methods:{
+        insertPessoa(){
+            this.pessoas.push(this.pessoa)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
